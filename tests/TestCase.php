c4fb3282a3a835939cb9229c49a4c259dc352f5b
Enea Dhack <enea.so@live.com>
2023-05-07 22:05:48 -0500

    test: environment

---
+++ tests/TestCase.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @author enea dhack <enea.so@live.com>
+ */
+
+declare(strict_types=1);
+
+namespace Vaened\CriteriaLanguage\Tests;
+
+use PHPUnit\Framework\TestCase as PhpUnitTestCase;
+
+class TestCase extends PhpUnitTestCase
+{
+}
