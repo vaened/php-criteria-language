7085eaeeeb77da047971ee6ee17cc28f46839159
Enea Dhack <enea.so@live.com>
2023-05-11 22:24:02 -0500

    test: improve types in keywords

--- tests/Keywords/KeywordTestCase.php
+++ tests/Keywords/KeywordTestCase.php
@@ -10,12 +10,18 @@ namespace Vaened\CriteriaLanguage\Tests\Keywords;
 use Vaened\CriteriaLanguage\Keyword;
 use Vaened\CriteriaLanguage\Tests\TestCase;
 
+use function gettype;
+
 abstract class KeywordTestCase extends TestCase
 {
     public function assertThatKeywordIsCorrect(Sentence $sentence, Keyword $keyword): void
     {
+        $expected  = $sentence->expected();
+        $formatted = $keyword->format($sentence->sentence());
+
         $this->assertTrue($keyword->canApplyFor($sentence->sentence()));
-        $this->assertEquals($sentence->expected(), $keyword->format($sentence->sentence()));
+        $this->assertEquals(gettype($expected), gettype($formatted));
+        $this->assertEquals($expected, $formatted);
     }
 
     public function assertThatKeywordIsInvalid(string $target, Keyword $keyword): void
